56ab31a216b60ab6f275b2f477dba56d5494f831
Horga Filip-Sebastian <filip.horga03@e-uvt.ro>
2024-11-14 16:40:55 +0200

    app created

---
+++ crm-frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>button</Button>
+    </div>
+  );
+}
+
+export default App;
